f015783113348f9d34cffc00977e5eade323a39d
Cintriano <danilofernandes9077@gmail.com>
2025-09-04 18:01:19 -0300

    feito-barra,botão,listagem

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,64 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Itask {
+  id: number
+  text: string
+  isChecked: boolean
 }
 
-export default App
+const inicialState: Itask[] = [
+  {
+    id: 1,
+    text: "Nova Tarefa",
+    isChecked: false
+  },
+  {
+    id: 1,
+    text: "Nova Tarefa2",
+    isChecked: false
+  }
+];
+
+function App() {
+
+  const [tasks, setTasks] = useState(inicialState);
+  const [inputName, setInputName] = useState("");
+
+  function addTask() {
+
+  }
+
+  return (
+
+    <div className="flex flex-col h-screen w-screen justify-center items-center">
+      <div className="mb-4">
+        <h1 className="text-4xl font-bold">Task-Manager</h1>
+      </div>
+  
+      <div className="flex flex-row gap-2">
+        <Input className="border border-gray-400 p-2 rounded-md w-96" />
+        <Button className="bg-blue-500 text-white p-2 rounded-md">New Task</Button>
+        <Button className="bg-gray-400 text-white p-2 rounded-md">Clean</Button>
+      </div>
+
+      <div className="mt-8">
+        {tasks.map((task) => {
+                return (
+                  <Card key={task.id} className="mb-2">
+                    <CardContent className="p-4">
+                      <p>{task.text}</p>
+                    </CardContent>
+                  </Card>
+                )
+              })}
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
